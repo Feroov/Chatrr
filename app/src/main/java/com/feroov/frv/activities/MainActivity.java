d5aa619317d4a990cd51bc18c5ad1220a009ff12
Feroov <feroovt@gmail.com>
2022-05-13 16:33:12 +0300

    Log out functionality

--- app/src/main/java/com/feroov/frv/activities/MainActivity.java
+++ app/src/main/java/com/feroov/frv/activities/MainActivity.java
@@ -2,15 +2,72 @@ package com.feroov.frv.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
 
-import com.feroov.frv.R;
+import com.feroov.frv.databinding.ActivityMainBinding;
+import com.feroov.frv.utilities.Constants;
+import com.feroov.frv.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+        setListeners();
+    }
+
+    private void setListeners()
+    {
+        binding.imageSignOut.setOnClickListener(v -> signOut());
+    }
+
+    private void loadUserDetails()
+    {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64. DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String message)
+    {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signOut()
+    {
+        showToast("Signing out...");
+        FirebaseFirestore database=FirebaseFirestore.getInstance();
+        DocumentReference documentReference=
+                database.collection(Constants.KEY_COLLECTION_USERS).document(
+                        preferenceManager.getString(Constants.KEY_USER_ID));
+
+        HashMap<String, Object> updates=new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused ->
+                {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                }).addOnFailureListener(e -> showToast("Unable to Sign Out!"));
     }
 }
