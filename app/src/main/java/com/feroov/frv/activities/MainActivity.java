9ce13740225880b051b01783f7fd074ad3be352a
Feroov <feroovt@gmail.com>
2022-05-15 18:41:50 +0300

    Final commit

--- app/src/main/java/com/feroov/frv/activities/MainActivity.java
+++ app/src/main/java/com/feroov/frv/activities/MainActivity.java
@@ -1,28 +1,45 @@
 package com.feroov.frv.activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.View;
 import android.widget.Toast;
 
+import com.feroov.frv.adapters.RecentConversationsAdapter;
 import com.feroov.frv.databinding.ActivityMainBinding;
+import com.feroov.frv.listeners.ConversionListener;
+import com.feroov.frv.models.ChatMessage;
+import com.feroov.frv.models.User;
 import com.feroov.frv.utilities.Constants;
 import com.feroov.frv.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements ConversionListener
 {
     private ActivityMainBinding binding;
     private PreferenceManager preferenceManager;
+    private List<ChatMessage> conversations;
+    private RecentConversationsAdapter conversationsAdapter;
+    private FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,9 +47,19 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        init();
         preferenceManager = new PreferenceManager(getApplicationContext());
         loadUserDetails();
         setListeners();
+        listenConversations();
+    }
+
+    private void init()
+    {
+        conversations = new ArrayList<>();
+        conversationsAdapter = new RecentConversationsAdapter(conversations, this);
+        binding.conversationRecyclerView.setAdapter(conversationsAdapter);
+        database = FirebaseFirestore.getInstance();
     }
 
     private void setListeners()
@@ -55,6 +82,71 @@ public class MainActivity extends AppCompatActivity
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
+    private void listenConversations()
+    {
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private final EventListener<QuerySnapshot> eventListener = (value, error) ->
+    {
+      if(error != null){ return; }
+      if(value != null)
+      {
+          for(DocumentChange documentChange : value.getDocumentChanges())
+          {
+              if(documentChange.getType() == DocumentChange.Type.ADDED)
+              {
+                  String senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                  String receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                  ChatMessage chatMessage = new ChatMessage();
+                  chatMessage.senderId = senderId;
+                  chatMessage.receiverId = receiverId;
+                  if(preferenceManager.getString(Constants.KEY_USER_ID).equals(senderId))
+                  {
+                      chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_RECEIVER_IMAGE);
+                      chatMessage.conversionName = documentChange.getDocument().getString(Constants.KEY_RECEIVER_NAME);
+                      chatMessage.conversionId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                  }
+                  else
+                  {
+                      chatMessage.conversionImage = documentChange.getDocument().getString(Constants.KEY_SENDER_IMAGE);
+                      chatMessage.conversionName = documentChange.getDocument().getString(Constants.KEY_SENDER_NAME);
+                      chatMessage.conversionId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                  }
+                  chatMessage.message = documentChange.getDocument().getString(Constants.KEY_LAST_MESSAGE);
+                  chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                  conversations.add(chatMessage);
+              }
+              else if(documentChange.getType() == DocumentChange.Type.MODIFIED)
+              {
+                  for(int i = 0; i < conversations.size(); i++)
+                  {
+                      String senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                      String receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                      if(conversations.get(i).senderId.equals(senderId) && conversations.get(i).receiverId.equals(receiverId))
+                      {
+                          conversations.get(i).message = documentChange.getDocument().getString(Constants.KEY_LAST_MESSAGE);
+                          conversations.get(i).dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                          break;
+                      }
+                  }
+              }
+          }
+          Collections.sort(conversations, (obj1, obj2) -> obj2.dateObject.compareTo(obj1.dateObject));
+          conversationsAdapter.notifyDataSetChanged();
+          binding.conversationRecyclerView.smoothScrollToPosition(0);
+          binding.conversationRecyclerView.setVisibility(View.VISIBLE);
+          binding.progressBar.setVisibility(View.GONE);
+      }
+    };
+
     private void signOut()
     {
         showToast("Signing out...");
@@ -72,5 +164,14 @@ public class MainActivity extends AppCompatActivity
                     startActivity(new Intent(getApplicationContext(), SignInActivity.class));
                     finish();
                 }).addOnFailureListener(e -> showToast("Unable to Sign Out!"));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onConversionClicked(User user)
+    {
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        intent.putExtra(Constants.KEY_USER, user);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/feroov/frv/listeners/ConversionListener.java
@@ -0,0 +1,8 @@
+package com.feroov.frv.listeners;
+
+import com.feroov.frv.models.User;
+
+public interface ConversionListener
+{
+    void onConversionClicked(User user);
+}
